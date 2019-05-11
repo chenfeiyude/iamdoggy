d15cee70a832e1836198693a6ae936f3bd843c2e
Feiyu <chenfeiyu0402@gmail.com>
2019-05-11 01:59:57 +0100

    init project

---
+++ src/main/java/com/iamdoggy/iamdoggy/IamdoggyApplication.java
@@ -0,0 +1,13 @@
+package com.iamdoggy.iamdoggy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamdoggyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamdoggyApplication.class, args);
+	}
+
+}
